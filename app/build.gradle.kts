f79d83a4ce7b7c866f9571f9428b7fa51d128a0e
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-07-13 02:33:01 -0700

    Feat: Bump target SDK to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     defaultConfig {
         applicationId = "me.zhanghai.android.beeshell"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 3
         versionName = "1.0.2"
         resValue("string", "app_version", "$versionName ($versionCode)")
