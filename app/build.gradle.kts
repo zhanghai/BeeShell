1ba52d97009256ee28442e3ffe646856003f78e4
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-07-13 02:05:15 -0700

    Build: Use Kotlin for build scripts
    And update compile SDK, NDK, build tools and Kotlin.

---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "me.zhanghai.android.beeshell"
+    compileSdk = 34
+    ndkVersion = "26.3.11579264"
+    buildToolsVersion = "34.0.0"
+    defaultConfig {
+        applicationId = "me.zhanghai.android.beeshell"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 3
+        versionName = "1.0.2"
+        resValue("string", "app_version", "$versionName ($versionCode)")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+    signingConfigs {
+        create("release") {
+            storeFile = System.getenv("STORE_FILE")?.let { rootProject.file(it) }
+            storePassword = System.getenv("STORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    packaging {
+        jniLibs {
+            keepDebugSymbols += "**/libbsh.so"
+            useLegacyPackaging = true
+        }
+    }
+}
+
+dependencies {
+    val kotlinxCoroutinesVersion = "1.6.4"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinxCoroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    val androidxLifecycleVersion = "2.5.1"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$androidxLifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$androidxLifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-common-java8:$androidxLifecycleVersion")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("dev.chrisbanes.insetter:insetter:0.6.0")
+    implementation("me.zhanghai.android.linenoise:library:1.0.1")
+    implementation("org.apache-extras.beanshell:bsh:2.0b6")
+}
